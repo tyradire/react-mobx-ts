4742fd89e4e027789bd9e86b08dc20ae7b615e11
Daniel Ugarov <tyradire@yandex.ru>
2024-06-12 16:50:26 +0300

    del: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/tyradire/react-mobx-ts",
+  base: "/react-mobx-ts",
   resolve: {
     alias: {
       components: '/src/components',
